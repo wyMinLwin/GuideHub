ec4c1b5ada8325c99e015f01f0c383555ff44f46
wyMinLwin <waiyanminlwin421@gmail.com>
2023-11-07 18:37:44 +0630

    :sparkles: Create Login Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'light':"#F2F2F2",
+        'bluejeans':'#5D9CEC'
+      }
     },
   },
   plugins: [],
