c9290786726a1e0ebd9f7256f7249e00b3d5f176
Han Ye Htun <HanYeHtun@fusionsol.com>
2023-11-20 11:02:00 +0630

    :bug: fix update api

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -59,12 +59,14 @@ export async function PUT(req: NextRequest) {
         const client: MongoClient = await clientPromise
         const db = client.db(process.env.DB_NAME)
 
-        const { _id, ...body } = await req.json()
+        const { _id, user, ...body } = await req.json()
         const filter = { _id: new ObjectId(_id) }
+
+        const updateDocument = { $set: body };
 
         const newTask = await db
             .collection('tasks')
-            .findOneAndReplace(filter, body, {
+            .findOneAndUpdate(filter, updateDocument, {
                 returnDocument: 'after',
             })
 
